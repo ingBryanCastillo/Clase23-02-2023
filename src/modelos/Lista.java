86d1d3929050c2f98ebcd5c8dd076ed76d19591e
umg <umg@HUEB3.labhue.local>
2023-02-23 20:23:11 -0600

    version 0.1

---
+++ src/modelos/Lista.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author umg
+ */
+public class Lista {
+Nodo primero;
+
+
+public void InsertarNodo(int dato)
+{
+  Nodo nodoNuevo = new Nodo(dato);
+  if(primero == null)
+  {
+      primero = nodoNuevo;
+  }
+  else 
+  {
+      nodoNuevo.siguiente = primero;
+      primero = nodoNuevo;
+  }
+}
+
+public void ListarNodos()
+{
+    Nodo Auxiliar;
+    Auxiliar = primero;
+    while(Auxiliar!=null)
+    {
+        System.out.println(Auxiliar.getDato());
+        Auxiliar = Auxiliar.siguiente;
+    }
+
+
+}
+
+}
